16cc4509a06498efe0b5804f7033bce2b45e4edc
Luis Ortiz <luchoaam97@gmail.com>
2022-01-21 20:09:32 -0500

    Taller 20 queries

--- src/main/java/ec/edu/uce/modelo/jpa/Guardia.java
+++ src/main/java/ec/edu/uce/modelo/jpa/Guardia.java
@@ -5,61 +5,72 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "guardia")// Es absoluyamente necesario cundo la tabla tiene un nombre diferente
+@Table(name = "guardia") // Es absoluyamente necesario cundo la tabla tiene un nombre diferente
 //Declarar los query de Guardia
-@NamedQuery(name="Guardia.buscarPorApellido",query ="select g from Guardia g where g.apellido=: valor")
+@NamedQueries({
+		@NamedQuery(name = "Guardia.buscarPorApellido", query = "select g from Guardia g where g.apellido=: valor"),
+		@NamedQuery(name = "Guardia.buscarPorApellido1", query = "select g from Guardia g where g.apellido=: valor"),
+		@NamedQuery(name = "Guardia.buscarPorApellido2", query = "select g from Guardia g where g.apellido=: valor") })
+@NamedQuery(name = "Guardia.buscarPorApellido", query = "select g from Guardia g where g.apellido=: valor")
 public class Guardia {
-	
-	
+
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_guardia1")
-	@SequenceGenerator(name="seq_guardia1", sequenceName = "seq_guardia1",allocationSize = 1)
+	@SequenceGenerator(name = "seq_guardia1", sequenceName = "seq_guardia1", allocationSize = 1)
 	@Id
-	@Column(name= "id")
+	@Column(name = "id")
 	private Integer id;
-	
-	@Column(name= "nombre")
+
+	@Column(name = "nombre")
 	private String nombre;
-	
-	@Column(name= "apellido")
+
+	@Column(name = "apellido")
 	private String apellido;
-	
-	@Column(name= "edificio")
+
+	@Column(name = "edificio")
 	private String edificio;
-	
-	//metodos set y get
+
+	// metodos set y get
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getEdificio() {
 		return edificio;
 	}
+
 	public void setEdificio(String edificio) {
 		this.edificio = edificio;
 	}
+
 	@Override
 	public String toString() {
 		return "Guardia [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", edificio=" + edificio + "]";
 	}
-	
 
 }
